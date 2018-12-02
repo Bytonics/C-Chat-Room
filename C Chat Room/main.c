fcce4290d0513b56440bb2bbde051591bb954988
Emen <skrubsquadproductions@gmail.com>
2018-12-02 13:16:43 -0500

    Initial Commit

---
+++ C Chat Room/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C Chat Room
+//
+//  Created by Oseriemen Ivbaze on 12/2/18.
+//  Copyright © 2018 Chat Room. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
